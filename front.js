353cd5e7069a59cd3e4755c4eaad60145d94f182
VEGAS <51318919+VegasYT@users.noreply.github.com>
2024-11-19 01:14:46 +0300

    Update front.js

--- front.js
+++ front.js
@@ -1,20 +1,24 @@
+const webAppUrl = "{{ web_app_url }}"
+const tg = window.Telegram?.WebApp;
+const initData = tg.initData;
+
 fetch(`${webAppUrl}/validate`, {
-                    method: 'POST',
-                    headers: {
-                        'Authorization': `tma ${initData}`,
-                        'Content-Type': 'application/json',
-                    },
-                    credentials: 'include',
-                })
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Ошибка при валидации');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    console.log(data)
-                })
-                .catch(error => {
-                    console.error('Ошибка:', error);
-                });
+    method: 'POST',
+    headers: {
+        'Authorization': `tma ${initData}`,
+        'Content-Type': 'application/json',
+    },
+    credentials: 'include',
+})
+.then(response => {
+    if (!response.ok) {
+        throw new Error('Ошибка при валидации');
+    }
+    return response.json();
+})
+.then(data => {
+    console.log(data)
+})
+.catch(error => {
+    console.error('Ошибка:', error);
+});
